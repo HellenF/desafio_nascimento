58216c18607676c145ddaf8ccb045a62006c9673
Hellen Ferreira <hellencfujo@gmail.com>
2020-11-18 15:27:19 -0300

    Add files via upload

--- Model.cs
+++ Model.cs
@@ -1,26 +1,35 @@
 
 
-using System;
+   using System;
+   using Main;
+   using System;
 
-class desafio_nascimento
-{
+   class desafio_nascimento
+   
+     {
 
-DateTime date1 = new DateTime(2000, 16, 11);
-DateTime date2 = new DateTime(2020, 17, 11;
-int result = DateTime.Compare(date1, date2);
-string relationship;
+         static void Main(string[] args)
+          {
+            Idade Idade = new Idade();
 
-if (result < 0)
-   relationship = "0";
-else if (result == 0)
-   relationship = "test";
-else
-   relationship = "testando";
+            Idade.dataDeNascimento = DateTime(2020, 11 , 18);
+            Idade.ImprimirDados();
+            }
+            public int64 idade
+            {
+            get 
+            {
+               return DateAndTime.DateDiff(DateInterval.Year, dataDeNascimento, DateTime.Now);
+            }
+      }
 
-Console.WriteLine("{0} {1} {2}", date1, relationship, date2);
 
-// The example displays the following output for en-us culture:
-//    8/1/2009 12:00:00 AM is earlier than 8/1/2009 12:00:00 PM
-    
-}
-}   
+            
+
+
+
+
+
+
+      
+   
